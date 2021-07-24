91be701db96f6e15c8d081b3930b6c3b43e57b3e
Olivier Leplus <leplus.olivier@gmail.com>
2021-07-24 17:19:41 +0200

    add todo

---
+++ api/tasks/index.js
@@ -0,0 +1,11 @@
+module.exports = async function (context, req) {
+    const header = req.headers['x-ms-client-principal'];
+    const encoded = Buffer.from(header, 'base64');
+    const decoded = encoded.toString('ascii');
+
+    context.res = {
+        body: {
+            clientPrincipal: JSON.parse(decoded),
+        },
+    };
+}
--- www/app.js
+++ www/app.js
@@ -1,4 +1,9 @@
 
+async function getTasks() {
+    const response = await fetch('/api/tasks');
+    const payload = await response.json();
+}
+
 async function getUser() {
     const response = await fetch('/api/users');
     const payload = await response.json();
@@ -7,3 +12,36 @@ async function getUser() {
     return clientPrincipal;
 }
 getUser();
+
+const taskform = document.querySelector('#new_task_form');
+const taskList = document.querySelector('#tasksList');
+
+taskform.addEventListener('submit', async (e) => {
+
+    e.preventDefault();
+    const newTaskInput = taskform.elements.new_task_input;
+
+    const response = await fetch('/api/tasks', {
+        method: 'POST',
+        body: new_task_input.value
+    });
+    const payload = await response.json();
+    console.log(payload);
+    const task = generateTask(newTaskInput.value);
+
+    taskList.appendChild(task);
+
+    newTaskInput.value = '';
+});
+
+function generateTask(task) {
+    const tmpl = `
+    <div class="task">
+        ${task}
+    </div>
+    `;
+    const range = document.createRange();
+    const fragment = range.createContextualFragment(tmpl);
+
+    return fragment;
+}
