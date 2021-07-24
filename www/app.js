9b13b5a03e86480a388ee3ce24d83ff0ee2629e1
Olivier Leplus <leplus.olivier@gmail.com>
2021-07-24 16:25:52 +0200

    fix(api) user => users

--- www/app.js
+++ www/app.js
@@ -1,6 +1,6 @@
 
 async function getUser() {
-    const response = await fetch('/api/user');
+    const response = await fetch('/api/users');
     const payload = await response.json();
     console.log(payload);
     const { clientPrincipal } = payload;
