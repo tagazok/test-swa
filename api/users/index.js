164a6900346157dc40781fc2525b72446786a158
Olivier Leplus <leplus.olivier@gmail.com>
2021-07-24 16:47:10 +0200

    return user

--- api/users/index.js
+++ api/users/index.js
@@ -1,13 +1,11 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const header = req.headers['x-ms-client-principal'];
+    const encoded = Buffer.from(header, 'base64');
+    const decoded = encoded.toString('ascii');
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: {
+            clientPrincipal: JSON.parse(decoded),
+        },
     };
 }
