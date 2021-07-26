f72ba5afcd96d260e27bb114303e8e2039c219d1
Olivier Leplus <leplus.olivier@gmail.com>
2021-07-26 17:39:40 +0200

    checklogin

---
+++ www/checklogin.js
@@ -0,0 +1,9 @@
+async function getUser() {
+    const reponse = await fetch('/.auth/me');
+    const payload = await reponse.json();
+    console.log(payload.clientPrincipal);
+    if (payload.clientPrincipal == null) {
+        window.location.replace = "/login.html";
+
+    }
+}
